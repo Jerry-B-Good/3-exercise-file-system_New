3ecd3c8cdf92028ebdad5d32ade6dab566306419
Good-E-Nuff <248106637+Jerry-B-Good@users.noreply.github.com>
2026-05-25 22:21:33 -0400

    Replaced image

--- obj/Debug/net10.0/3-exercise-file-system_New.AssemblyInfo.cs
+++ obj/Debug/net10.0/3-exercise-file-system_New.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("3-exercise-file-system_New")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a737ca032f11dfbc2489a67c7579ba8494bf0bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39faed048404b6a4b4f27128e280884fd7a7d986")]
 [assembly: System.Reflection.AssemblyProductAttribute("3-exercise-file-system_New")]
 [assembly: System.Reflection.AssemblyTitleAttribute("3-exercise-file-system_New")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
